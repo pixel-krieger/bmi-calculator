d85e028f353c3ede2335690f7b811d3db440bd03
sr-pixel <wilbergg.2001@gmail.com>
2021-01-21 00:33:37 -0500

    Installed express and body-parser. Implemented response for post method.

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
     res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/", function(req, res){
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var bmi = weight/Math.pow(height, 2);
+    res.send("Your BMI is: "+bmi);
 });
 
 app.listen(3000);
